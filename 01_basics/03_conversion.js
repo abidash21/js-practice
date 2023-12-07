ac3806e4a820ff6bcecf8a7d38d6558e7e95f14e
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2023-12-07 13:59:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let num="123wer"
+console.log(typeof(num))
+let convertNum=Number(num);
+console.log(typeof convertNum)
+console.log(convertNum)
+
+//if we convert a string to number where the string is not a pure number,it will converted to a number but its value will be NAN
+
+//NAN-not a number
+
+//when converting a datatype to another datatype the name of the data type is used as a method with the first letter as uppercase
+
+/* 
+"33"=>33
+"33abc"=>NaN
+true=>1
+null=>0
+undefined=>NaN
+ */
+
+/*
+1=>true;0=>false
+"abi"=>true;""=>false
+*/
