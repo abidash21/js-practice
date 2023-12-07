891a82a34f729ab428be1f530f8790ea5311fe78
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2023-12-07 14:22:48 +0000

    operations on datatype

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,15 @@
+console.log(2**3); //pow(2,3)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+2+"2")
+console.log("1"+2+2)
+
+console.log(+true);
+console.log(+"");
+
+/*
++true=>1
++""=>0 
+*/
+
