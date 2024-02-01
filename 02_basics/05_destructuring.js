c0951c1c1c14c2ba22f5f56a4ff7dd139268e8e1
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-02-01 03:27:42 +0000

    commit object destructuring

---
+++ 02_basics/05_destructuring.js
@@ -0,0 +1,18 @@
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+console.log(courseInstructor)
+console.log(instructor)
+
+//JSON
+// {
+//     "name" : "Abinash",
+//     "price" : "999",
+//     "course": "JS in hindi"
+// }
+
