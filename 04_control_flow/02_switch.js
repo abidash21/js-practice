34afd848c932169e347f66cb07d49a17a7c32872
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-02-04 09:10:39 +0000

    commit switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,21 @@
+
+month = 3
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        //break;
+    case 4:
+        console.log("april")
+        //break;
+
+    default:
+        console.log("default")
+        break;
+}
+// if no break statement then all lines would be executed after condition matching
