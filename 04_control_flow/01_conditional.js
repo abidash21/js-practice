598c2e037a9c55d7623912202fcab50e61df7a90
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-02-04 07:23:27 +0000

    commit conditional

---
+++ 04_control_flow/01_conditional.js
@@ -0,0 +1,29 @@
+//if (condition)
+
+if (true){
+    //output
+}
+//
+if (true){
+    //output
+} else {
+    //output
+}
+//
+if (true){
+    //output
+} else if (false){
+    //output
+} else {
+    //output
+}
+
+//>,<,>=,<=,==,!=,===,!==
+//===(strict equals)-type checking
+//ex: 2=="2"  condition is true,values are same
+// 2==="2" condition is false, type is not equal
+
+if (true)  console.log("abinash") //no {},implicit scope
+
+//if (condition && condition && condition)
+//if (condition || condition || condition)
