6a393a92ae007359813d77da06ff5210e3e187cf
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-02-04 09:44:14 +0000

    commit loop

---
+++ 05_Iterations/01_loop.js
@@ -0,0 +1,18 @@
+//for loop
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+//break continue
+
+
+//while loop
+while (condition) {
+    
+}
+
+
+//do while loop
+do {
+    
+} while (condition);
