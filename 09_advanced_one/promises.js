038da03ac862217559c8f343370fd36224e1e680
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-02-29 00:41:06 +0000

    commit promise

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -29,3 +29,69 @@ const promiseThree = new Promise((resolve,reject) => {
 }).then( (user) => {
         console.log(user)
 })
+
+
+const promiseFour = new Promise( (resolve,reject) => {
+    setInterval(() => {
+        let error=false;
+        if (!error){
+            resolve({name:'Abinash',password:'1234'})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour.then((user)=>{
+    console.log(user)
+    return user.name
+}).then((username)=>{
+    console.log(username)
+}).catch((err)=>{
+    console.log(err);
+}).finally(()=>{
+    console.log('The promise is either resolved or rejected')
+})
+
+
+const promiseFive = new Promise( (resolve,reject) => {
+    setInterval(() => {
+        let error=true;
+        if (!error){
+            resolve({name:'Javascript',password:'1234'})
+        } else {
+            reject('ERROR: Js went wrong')
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data=await response.json()
+//         console.log(data)
+//     } catch (err) {
+//         console.log('Error',err)
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>  {return response.json()})
+.then((data) => {console.log(data)}).catch((error) => console.log(error))
+
+
+
